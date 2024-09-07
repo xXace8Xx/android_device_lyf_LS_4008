d7fcdd88e5529ebb290729ee45c3aad82c3d6ebc
Harish Kashyap <138990075+harishkashyap8@users.noreply.github.com>
2024-09-07 16:40:07 +0530

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -6,3 +6,5 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
 
+# Dont know if this works or not
+#LOCAL_CFLAGS += -Wl,--strip-all, -Wl,--gc-sections
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,6 +3,11 @@ USE_CAMERA_STUB := true
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := SP8835EB	# Got it from extract_dtb
 TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Might save storage
+TW_DEFAULT_LANGUAGE := en-US
+
 
 # Platform
 TARGET_BOARD_PLATFORM := sc8830
@@ -26,7 +31,13 @@ TARGET_CPU_SMP := true
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SP8835EB
+
+# Trying compressinon just like in kernel(might or might not work)
+#BOARD_RAMDISK_COMPRESS := lzma
+#TARGET_RECOVERY_FSTYPE := ext4
+#TARGET_RECOVERY_SIZE := 15M
+
 
 ## Filesystem
 BOARD_DATA_DEVICE := /dev/block/mmcblk0p24
@@ -41,12 +52,11 @@ BOARD_CACHE_FILESYSTEM_OPTIONS := rw
 
 
 # Partition info
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x15728640
-
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x15728640
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x1939865600
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x5111790592
-BOARD_CACHEIMAGE_PARTITION_SIZE := 0x157286400
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x000F00000	# size might be 0xF02800
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x000F00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x073A00000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x130AFBC00
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x009600000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/FortuneShip/LS_4008/kernel
@@ -75,12 +85,94 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/sprd_backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 200
+TW_DEFAULT_BRIGHTNESS := 162
 TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_INCLUDE_NTFS_3G := true
-TW_EXCLUDE_SUPERSU := true
-TW_EXTRA_LANGUAGES := true
+
+
 TW_USE_NEW_MINADBD := true
 
+# Screen resolution
+#TW_SCREEN_HEIGHT := 800
+#TW_SCREEN_WIDTH := 480
+
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+
+# debug
+#BOARD_HAS_NO_REAL_SDCARD := true
+
+# Enable verbose logging
+#TW_DEBUG := true
+TW_NO_SCREEN_BLANK := 1
+#TW_INCLUDE_DEBUG := 1
+
+# Ensure the log directory is writable
+TW_RECOVERY_LOG_PATH := /tmp/recovery.log
+TW_USE_COMPRESSION := true
+
+# Custom log paths (optional)
+TW_LOG_STORAGE := /data/media/0/TWRP
+
+
+
+## EXCLUDE to Save disk space
+TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+TW_EXCLUDE_APP_MANAGER := true
+TW_EXCLUDE_MULTIUSER := true
+TW_EXCLUDE_TREBLE := true
+TW_EXCLUDE_FBE := true
+
+TW_INCLUDE_NTFS_3G := false
+TW_EXCLUDE_SUPERSU := true
+TW_EXTRA_LANGUAGES := false	# Might save space
+TWRP_INCLUDE_LOGCAT := false
+
+
+TW_EXCLUDE_MTP := true
+TW_EXCLUDE_SDEXT := true
+TW_EXCLUDE_ADB := true
+TW_EXCLUDE_USB_STORAGE := true
+TW_NO_USB_STORAGE := true
+
+
+TW_EXCLUDE_DOWNLOAD_MODE := true
+TW_HAS_DOWNLOAD_MODE := false
+#TW_HAS_DOWNLOAD_MODE := true	# for samsung devices only
+
+TW_EXCLUDE_CRYPTO := true
+TW_EXCLUDE_OPENRECOVERY_SCRIPT := true
+TW_EXCLUDE_HAPTICS := true
+TW_EXCLUDE_BLKID := true
+TW_EXCLUDE_BATTERY_PERCENT := true
+
+
+TW_NO_CPU_TEMP := true
+# disables things like sdcard partitioning and may save you some space if TWRP isn't fitting in your recovery patition
+BOARD_HAS_NO_REAL_SDCARD := true
+
+
+# copied from device_android_samsung_galaxyl
+USE_NO_TORCH := true
+TW_NO_EXFAT := true
+TW_NO_EXFAT_FUSE := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+TARGET_BOOTANIMATION_USE_RGB565 := true
+
+# This dont work
+#TARGET_GLOBAL_CFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp -Oz -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables -fomit-frame-pointer -fmerge-all-constants -flto
+#TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon -mfloat-abi=softfp -Oz -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables -fomit-frame-pointer -fmerge-all-constants -flto
+
+#TARGET_GLOBAL_CFLAGS += -Oz -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables -fomit-frame-pointer -fmerge-all-constants -flto
+#TARGET_GLOBAL_CPPFLAGS += -Oz -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables -fomit-frame-pointer -fmerge-all-constants -flto
+#TARGET_GLOBAL_LDFLAGS += -Wl,--gc-sections -Wl,--strip-all #-Wl,--icf=all -flto -Wl,-Oz
+
+#TWRP_EVENT_LOGGING := false
+#TW_INCLUDE_FB2PNG := false
+#TW_USE_TOOLBOX := true
+
+
+#TW_NO_SCREEN_TIMEOUT := true
+#TW_DISABLE_TTF:= true	TW_DISABLE_TTF support has been deprecated in TWRP.
+#TW_OEM_BUILD := true	# consumes space
 
